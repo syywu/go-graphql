172633251fea1873754ce2bd2c87b912e6183f59
Samantha Wu <samanthayywu@gmail.com>
2023-02-27 17:00:42 +0000

    setup and chi installed

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
+
+	r := chi.NewRouter()
+
+	fmt.Println("listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
