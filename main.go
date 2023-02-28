77ad91c5aa026c02063f2e088f32bc8310f631e7
Samantha Wu <samanthayywu@gmail.com>
2023-02-28 13:13:52 +0000

    structs examples and db connection

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,65 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+// comments
+/*
+  {
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+*/
+
+// posts
+/*
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+*/
+
+// users
+/*
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+*/
+
 func main() {
+
+	db, err := sql.Open("postgres", "postgres://user:password@localhost/graphql-postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal("Cannot connect to db", err)
+
+	} else {
+		log.Println("db connection established")
+	}
+
+	defer db.Close()
 
 	r := chi.NewRouter()
 
