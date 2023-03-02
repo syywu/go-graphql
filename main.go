af9843e49da0e195b41106782db4f1a361ee9156
Samantha Wu <samanthayywu@gmail.com>
2023-03-02 12:25:38 +0000

    mutation folder

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/syywu/go-graphql/db"
 	"github.com/syywu/go-graphql/models"
+	"github.com/syywu/go-graphql/mutation"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/graphql-go/graphql"
@@ -41,79 +41,6 @@ import (
     }
   },
 */
-
-var mutationType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "Mutation",
-	Fields: graphql.Fields{
-		"create": &graphql.Field{
-			Type:        models.PostType,
-			Description: "Create a New Post",
-			Args: graphql.FieldConfigArgument{
-				"userid": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
-				"title":  &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
-				"body":   &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				db := db.OpenConnection()
-				userid, _ := p.Args["userid"].(int)
-				title, _ := p.Args["title"].(string)
-				body, _ := p.Args["body"].(string)
-				var post models.Post
-				_, err := db.Exec("INSERT INTO posts (userid, title, body) VALUES ($1, $2, $3) RETURNING id", userid, title, body)
-				if err != nil {
-					return nil, err
-				}
-				defer db.Close()
-				return post, nil
-			},
-		},
-		"update": &graphql.Field{
-			Type:        models.PostType,
-			Description: "Update a Post",
-			Args: graphql.FieldConfigArgument{
-				"id":     &graphql.ArgumentConfig{Type: graphql.Int},
-				"userid": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
-				"title":  &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
-				"body":   &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				db := db.OpenConnection()
-				id, _ := p.Args["id"].(int)
-				userid, _ := p.Args["userid"].(int)
-				title, _ := p.Args["title"].(string)
-				body, _ := p.Args["body"].(string)
-				row, err := db.Exec("UPDATE posts SET userid = $1, title = $2, body = $3 WHERE id = $4 RETURNING *", userid, title, body, id)
-				if err != nil {
-					return nil, err
-				}
-				row.RowsAffected()
-				defer db.Close()
-				return row, nil
-			},
-		},
-		"delete": &graphql.Field{
-			Type:        graphql.Boolean,
-			Description: "Delete a Post",
-			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				db := db.OpenConnection()
-				id, _ := p.Args["id"].(int)
-				row, err := db.Exec("DELETE FROM posts WHERE id =$1", id)
-				if err != nil {
-					return nil, err
-				}
-				defer db.Close()
-				rowsAffected, _ := row.RowsAffected()
-				if rowsAffected == 0 {
-					return nil, errors.New("post not found")
-				}
-				return true, nil
-			},
-		},
-	},
-})
 
 // define our schema- define what fields to return to us for when making queries
 var queryType = graphql.NewObject(graphql.ObjectConfig{
@@ -175,7 +102,7 @@ func main() {
 
 	// rootQuery- where to start
 	// defines a schema config
-	schemaConfig := graphql.SchemaConfig{Query: queryType, Mutation: mutationType}
+	schemaConfig := graphql.SchemaConfig{Query: queryType, Mutation: mutation.MutationType}
 	// creates schema
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
@@ -187,8 +114,9 @@ func main() {
 	// query- to get back our query
 	query := `
 	{
-  		posts{
+		posts{
 			id
+			title
 		}
 	}
 	`
---
+++ mutation/posts.go
@@ -0,0 +1,82 @@
+package mutation
+
+import (
+	"errors"
+
+	"github.com/graphql-go/graphql"
+	"github.com/syywu/go-graphql/db"
+	"github.com/syywu/go-graphql/models"
+)
+
+var MutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"create": &graphql.Field{
+			Type:        models.PostType,
+			Description: "Create a New Post",
+			Args: graphql.FieldConfigArgument{
+				"userid": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
+				"title":  &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+				"body":   &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				db := db.OpenConnection()
+				userid, _ := p.Args["userid"].(int)
+				title, _ := p.Args["title"].(string)
+				body, _ := p.Args["body"].(string)
+				var post models.Post
+				_, err := db.Exec("INSERT INTO posts (userid, title, body) VALUES ($1, $2, $3) RETURNING id", userid, title, body)
+				if err != nil {
+					return nil, err
+				}
+				defer db.Close()
+				return post, nil
+			},
+		},
+		"update": &graphql.Field{
+			Type:        models.PostType,
+			Description: "Update a Post",
+			Args: graphql.FieldConfigArgument{
+				"id":     &graphql.ArgumentConfig{Type: graphql.Int},
+				"userid": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
+				"title":  &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+				"body":   &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				db := db.OpenConnection()
+				id, _ := p.Args["id"].(int)
+				userid, _ := p.Args["userid"].(int)
+				title, _ := p.Args["title"].(string)
+				body, _ := p.Args["body"].(string)
+				row, err := db.Exec("UPDATE posts SET userid = $1, title = $2, body = $3 WHERE id = $4 RETURNING *", userid, title, body, id)
+				if err != nil {
+					return nil, err
+				}
+				row.RowsAffected()
+				defer db.Close()
+				return row, nil
+			},
+		},
+		"delete": &graphql.Field{
+			Type:        graphql.Boolean,
+			Description: "Delete a Post",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int)},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				db := db.OpenConnection()
+				id, _ := p.Args["id"].(int)
+				row, err := db.Exec("DELETE FROM posts WHERE id =$1", id)
+				if err != nil {
+					return nil, err
+				}
+				defer db.Close()
+				rowsAffected, _ := row.RowsAffected()
+				if rowsAffected == 0 {
+					return nil, errors.New("post not found")
+				}
+				return true, nil
+			},
+		},
+	},
+})
