8bfc74620f19622cff6fbdd39d2f347f83dc5fa8
Samantha Wu <samanthayywu@gmail.com>
2023-03-03 10:07:32 +0000

    user strcut

--- main.go
+++ main.go
@@ -15,32 +15,35 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// users
-/*
-  {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  },
-*/
+type User struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Username string  `json:"username"`
+	Email    string  `json:"email"`
+	Address  Address `json:"address"`
+	Phone    string  `json:"phone"`
+	Website  string  `json:"website"`
+	Company  Company `json:"company"`
+}
+
+type Address struct {
+	Street  string `json:"street"`
+	Suite   string `json:"suite"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+	Geo     Geo    `json:"geo"`
+}
+
+type Geo struct {
+	Lat string `json:"lat"`
+	Lng string `json:"lng"`
+}
+
+type Company struct {
+	Name        string `json:"name"`
+	Catchphrase string `json:"catchphrase"`
+	Bs          string `json:"bs"`
+}
 
 func main() {
 
