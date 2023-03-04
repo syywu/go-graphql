322c3853abf1168d5099408cab1dd49a549c63ca
Samantha Wu <samanthayywu@gmail.com>
2023-03-04 22:41:22 +0000

    updateUser

--- mutation/posts.go
+++ mutation/posts.go
@@ -77,6 +77,55 @@ var MutationType = graphql.NewObject(graphql.ObjectConfig{
 				return post, nil
 			},
 		},
+		"updateUser": &graphql.Field{
+			Type:        models.UserType,
+			Description: "Create a New User",
+			Args: graphql.FieldConfigArgument{
+				"id":                   &graphql.ArgumentConfig{Type: graphql.ID},
+				"name":                 &graphql.ArgumentConfig{Type: graphql.String},
+				"username":             &graphql.ArgumentConfig{Type: graphql.String},
+				"email":                &graphql.ArgumentConfig{Type: graphql.String},
+				"address_street":       &graphql.ArgumentConfig{Type: graphql.String},
+				"address_suite":        &graphql.ArgumentConfig{Type: graphql.String},
+				"address_city":         &graphql.ArgumentConfig{Type: graphql.String},
+				"address_zipcode":      &graphql.ArgumentConfig{Type: graphql.String},
+				"address_geo_lat":      &graphql.ArgumentConfig{Type: graphql.String},
+				"address_geo_lng":      &graphql.ArgumentConfig{Type: graphql.String},
+				"phone":                &graphql.ArgumentConfig{Type: graphql.String},
+				"website":              &graphql.ArgumentConfig{Type: graphql.String},
+				"company_name":         &graphql.ArgumentConfig{Type: graphql.String},
+				"company_catch_phrase": &graphql.ArgumentConfig{Type: graphql.String},
+				"company_bs":           &graphql.ArgumentConfig{Type: graphql.String},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				db := db.OpenConnection()
+				id, _ := p.Args["id"].(string)
+				name, _ := p.Args["name"].(string)
+				username, _ := p.Args["username"].(string)
+				email, _ := p.Args["email"].(string)
+				address_street, _ := p.Args["address_street"].(string)
+				address_suite, _ := p.Args["address_suite"].(string)
+				address_city, _ := p.Args["address_city"].(string)
+				address_zipcode, _ := p.Args["address_zipcode"].(string)
+				address_geo_lat, _ := p.Args["address_geo_lat"].(string)
+				address_geo_lng, _ := p.Args["address_geo_lng"].(string)
+				phone, _ := p.Args["phone"].(string)
+				website, _ := p.Args["website"].(string)
+				company_name, _ := p.Args["company_name"].(string)
+				company_catch_phrase, _ := p.Args["company_catch_phrase"].(string)
+				company_bs, _ := p.Args["company_bs"].(string)
+				row, err := db.Exec("UPDATE users SET name = $1, username= $2, email= $3, address_street= $4,address_suite= $5, address_city=$6, address_zipcode=$7, address_geo_lat=$8, address_geo_lng= $9, phone= $10, website= $11, company_name= $12, company_catch_phrase= $13, company_bs=$14 WHERE id = $15 RETURNING id", name, username, email, address_street, address_suite, address_city, address_zipcode, address_geo_lat, address_geo_lng, phone, website, company_name, company_catch_phrase, company_bs, id)
+				if err != nil {
+					return nil, err
+				}
+				defer db.Close()
+				rowsAffected, _ := row.RowsAffected()
+				if rowsAffected == 0 {
+					return nil, errors.New("post not found")
+				}
+				return row, nil
+			},
+		},
 		"updatePost": &graphql.Field{
 			Type:        models.PostType,
 			Description: "Update a Post",
