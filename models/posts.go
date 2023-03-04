2708b8b19ca30653d9e68fb1a472c0ea71b0f437
Samantha Wu <samanthayywu@gmail.com>
2023-03-04 14:36:12 +0000

    associate post userid to users id

--- models/posts.go
+++ models/posts.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"log"
+
 	"github.com/graphql-go/graphql"
+	"github.com/syywu/go-graphql/db"
 )
 
 type Post struct {
@@ -15,10 +18,25 @@ var PostType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Post",
 		Fields: graphql.Fields{
-			"id":     &graphql.Field{Type: graphql.Int},
-			"userId": &graphql.Field{Type: graphql.Int},
-			"title":  &graphql.Field{Type: graphql.String},
-			"body":   &graphql.Field{Type: graphql.String},
+			"id":    &graphql.Field{Type: graphql.Int},
+			"title": &graphql.Field{Type: graphql.String},
+			"body":  &graphql.Field{Type: graphql.String},
+			"userId": &graphql.Field{
+				Type: UserType,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if parent, ok := p.Source.(*Post); ok {
+						user := &User{}
+						db := db.OpenConnection()
+						err := db.QueryRow("SELECT * FROM users WHERE id = $1", parent.UserId).Scan(&user.Name, &user.Username, &user.Email, &user.Address, &user.Phone, &user.Website, &user.Company)
+						if err != nil {
+							log.Fatal(err)
+						}
+						defer db.Close()
+						return user, nil
+					}
+					return nil, nil
+				},
+			},
 		},
 	},
 )
